00889bc9ff0a85d74c0ff0aa0cc69953b9cd1f91
dimasdk <dimasdwikurniawan94@gmail.com>
2024-06-14 09:13:08 +0700

    Assessment 3

--- app/src/main/java/org/d3if0041/assessment/inventorytoko/screen/main/ProfilDialog.kt
+++ app/src/main/java/org/d3if0041/assessment/inventorytoko/screen/main/ProfilDialog.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -15,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -53,8 +55,7 @@ fun ProfilDialog (
                     contentScale = ContentScale.Crop,
                     placeholder = painterResource(id = R.drawable.loading_img),
                     error = painterResource(id = R.drawable.baseline_account_circle_24),
-                    modifier = Modifier.size(100.dp)
-                )
+                    modifier = Modifier.size(100.dp).clip(CircleShape)                )
                 Text(
                     text = user.name,
                     maxLines = 1,
